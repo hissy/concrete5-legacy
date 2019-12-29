659b182801d35b895fd0b795033b2a3036452dda
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2019-12-29 22:31:16 +0900

    Make it enable to build patch version

--- build/tasks/build-release/clean.js
+++ build/tasks/build-release/clean.js
@@ -7,7 +7,7 @@ module.exports = function(grunt, config, parameters, done) {
 
 	try {
 		var fs = require('fs'),
-			path = "./release/concrete5-legacy-master/web",
+			path = "./release/concrete5-legacy-patch-5.6.3.5.1/web",
 			cleanFiles = [".DS_Store", ".git", ".gitignore"],
 			walk = function(directory, onComplete) {
 				fs.readdir(directory, function(err, list) {
